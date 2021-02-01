4d123286eec3a43206474ae772ed51612b5d2ed2
electronhead <gary.beaver@electronhead.com>
2021-02-01 15:27:14 -0800

    refactored uvicorn unit testing a bit

--- base/test/fixtures.py
+++ base/test/fixtures.py
@@ -8,12 +8,12 @@ from pyrambium.base.service.mothership import Mothership
 from pyrambium.base.service.action import FileHeartbeat
 from pyrambium.base.service.scheduler import TimelyScheduler
 from pyrambium.base.service.continuous import Continuous
+from pyrambium.base.test.server import UvicornTestServer
+from pyrambium.app import main_temp
 
 @pytest.fixture
 def friends(tmp_path):
-    """
-    returns a tuple of useful test objects
-    """
+    """ returns a tuple of useful test objects """
     def stuff():
         # want a fresh tuple from the fixture
         saved_dir = str(tmp_path)
@@ -28,24 +28,23 @@ def friends(tmp_path):
 
 @pytest.fixture
 def port():
-    """
-    returns a random port number in case the unit test system runs test in parallel
-    """
-    return random.randint(5000,8000)
+    """ returns a random port number in case the unit test system runs test in parallel """
+    return random.randint(5001,7999)
 
 @pytest.fixture
 def host():
-    """
-    the host used in unit tests
-    """
+    """ the host used in unit tests """
     return '127.0.0.1'
 
 @pytest.fixture
 async def startup_and_shutdown_uvicorn(host, port):
-    from pyrambium.base.test.server import UvicornTestServer
-    from pyrambium.app import main_temp
     """Start server as test fixture and tear down after test"""
     uvicorn_server = UvicornTestServer(app=main_temp.app, host=host, port=port)
     await uvicorn_server.up()
     yield
     await uvicorn_server.down()
+
+@pytest.fixture
+def base_url(host, port):
+    """ base url for unit test invocation of api """
+    return f"http://{host}:{port}"
--- base/test/test_uvicorn.py
+++ base/test/test_uvicorn.py
@@ -15,166 +15,131 @@ from pyrambium.base.service.action import FileHeartbeat, Action
 from pyrambium.base.service.scheduler import TimelyScheduler, Scheduler
 from pyrambium.base.service.util import FilePathe, ResolveBody
 from pyrambium.app.workbench import ApiHost
-from pyrambium.base.test.fixtures import port, host, startup_and_shutdown_uvicorn
+from pyrambium.base.test.fixtures import port, host, startup_and_shutdown_uvicorn, base_url
 from pydantic import BaseModel
 from httpx import AsyncClient
 
 @pytest.mark.asyncio
-async def test_uvicorn_1(startup_and_shutdown_uvicorn, port, host):
+async def test_uvicorn_1(startup_and_shutdown_uvicorn, base_url):
     """
     test to see if uvicorn responds to http requests inside the function
     """
-    async with AsyncClient(base_url=f"http://{host}:{port}/") as ac:
+    async with AsyncClient(base_url=f"{base_url}/") as ac:
         response = await ac.get("/")
         assert response.status_code == 200
         assert response.json() == 'Pyrambium API server started (unit test)'
 
 @pytest.mark.asyncio
-async def test_uvicorn_2(startup_and_shutdown_uvicorn, port, host, tmp_path):
+async def test_uvicorn_2(startup_and_shutdown_uvicorn, base_url, tmp_path):
     """
-    first N lines of test_uvicorn_3
+    add action and scheduler
     """
-    base_url = compute_base_url(host, port)
+    await reset_mothership_continuous(base_url, str(tmp_path))
+
     output_file = str(tmp_path / 'output.txt')
-    xtra={'port':port, 'ip_addr': host}
+    xtra={'base_url':base_url}
     action = FileHeartbeat(file=output_file, xtra=xtra)
     scheduler = TimelyScheduler(interval=1)
 
-    saved_dir = FilePathe(path=str(tmp_path))
-    response = await put(base_url=base_url, path='/mothership/saved_dir', data=saved_dir)
-    assert response.status_code == 200, 'failed to set saved_dir'
-    response = await get(base_url=base_url, path='/mothership/saved_dir')
-    assert response.status_code == 200, 'failed to get saved_dir'
-    saved_saved_dir = ResolveBody(FilePathe)(response.json())
-    assert saved_saved_dir == saved_dir
-    
-    response = await get(base_url=base_url, path='/mothership/clear')
-    assert response.status_code == 200, 'failed to get clear mothership'
-    response = await get(base_url=base_url, path='/jobs/stop') # likely already stopped
-
-    response = await put(base_url=base_url, path='/actions/foo', data=action)
-    assert response.status_code == 200, f"failed to put action (foo)"
-    response = await get(base_url, path='/actions/foo')
-    assert response.status_code == 200, f"failed to get action (foo)"
-    retrieved_action = ResolveBody(Action)(response.json())
-    assert isinstance(retrieved_action, Action), str(type(retrieved_action))
-
-    response = await put(base_url=base_url, path='/schedulers/bar', data=scheduler)
-    assert response.status_code == 200, f"failed to put scheduler (bar)"
-    response = await get(base_url, path='/schedulers/bar')
-    assert response.status_code == 200, f"failed to get scheduler (bar)"
-    retrieved_scheduler = ResolveBody(Scheduler)(response.json())
-    assert isinstance(retrieved_scheduler, Scheduler), str(type(retrieved_scheduler))
+    await add_action(base_url=base_url, name='foo', action=action)
+    await add_scheduler(base_url=base_url, name='bar', scheduler=scheduler)
 
 @pytest.mark.asyncio
-async def test_uvicorn_3(startup_and_shutdown_uvicorn, port, host, tmp_path):
+async def test_uvicorn_3(startup_and_shutdown_uvicorn, base_url, tmp_path):
     """
-    first N lines of test_uvicorn_4
+    add action and scheduler and then schedule the action
     """
-    base_url = compute_base_url(host, port)
+    await reset_mothership_continuous(base_url, str(tmp_path))
+
     output_file = str(tmp_path / 'output.txt')
-    xtra={'port':port, 'ip_addr': host}
+    xtra={'base_url':base_url}
     action = FileHeartbeat(file=output_file, xtra=xtra)
     scheduler = TimelyScheduler(interval=1)
 
-    saved_dir = FilePathe(path=str(tmp_path))
-    response = await put(base_url=base_url, path='/mothership/saved_dir', data=saved_dir)
-    assert response.status_code == 200, 'failed to set saved_dir'
-    response = await get(base_url=base_url, path='/mothership/saved_dir')
-    assert response.status_code == 200, 'failed to get saved_dir'
-    saved_saved_dir = ResolveBody(FilePathe)(response.json())
-    assert saved_saved_dir == saved_dir
-
-    response = await get(base_url=base_url, path='/mothership/clear')
-    assert response.status_code == 200, 'failed to get clear mothership'
-    response = await get(base_url=base_url, path='/jobs/stop') # likely already stopped
-
-    response = await put(base_url=base_url, path='/actions/foo', data=action)
-    assert response.status_code == 200, f"failed to put action (foo)"
-    response = await get(base_url, path='/actions/foo')
-    assert response.status_code == 200, f"failed to get action (foo)"
-    retrieved_action = ResolveBody(Action)(response.json())
-    assert isinstance(retrieved_action, Action), str(type(retrieved_action))
-
-    response = await put(base_url=base_url, path='/schedulers/bar', data=scheduler)
-    assert response.status_code == 200, f"failed to put scheduler (bar)"
-    response = await get(base_url, path='/schedulers/bar')
-    assert response.status_code == 200, f"failed to get scheduler (bar)"
-    retrieved_scheduler = ResolveBody(Scheduler)(response.json())
-    assert isinstance(retrieved_scheduler, Scheduler), str(type(retrieved_scheduler))
-
-    response = await get(base_url=base_url, path='/schedulers/bar/actions/foo')
-    assert response.status_code == 200, f"failed to schedule action (foo) using scheduler (bar)"
-
-    response = await get(base_url, '/jobs/count')
-    assert response.status_code == 200, 'failed to retrieve job count'
-    job_count = response.json()['job_count']
-    assert job_count == 1, 'expecting a job count of 1'
+    await add_action(base_url=base_url, name='foo', action=action)
+    await add_scheduler(base_url=base_url, name='bar', scheduler=scheduler)
+    await schedule_action(base_url=base_url, action_name='foo', scheduler_name='bar')
+    await assert_job_count(base_url, 1)
 
 @pytest.mark.asyncio
-async def test_uvicorn_4(startup_and_shutdown_uvicorn, port, host, tmp_path):
+async def test_uvicorn_4(startup_and_shutdown_uvicorn, base_url, tmp_path):
     """
-    tests the scheduling and running of a FileHeartbeat
+    add action and scheduler, run continuous, and then make sure the action produced file output
     """
-    base_url = compute_base_url(host, port)
+    await reset_mothership_continuous(base_url, str(tmp_path))
+
     output_file = str(tmp_path / 'output.txt')
-    xtra={'port':port, 'ip_addr': host}
+    xtra={'base_url':base_url}
     action = FileHeartbeat(file=output_file, xtra=xtra)
     scheduler = TimelyScheduler(interval=1)
 
-    saved_dir = FilePathe(path=str(tmp_path))
-    response = await put(base_url=base_url, path='/mothership/saved_dir', data=saved_dir)
-    assert response.status_code == 200, 'failed to set saved_dir'
-    response = await get(base_url=base_url, path='/mothership/saved_dir')
-    assert response.status_code == 200, 'failed to get saved_dir'
-    saved_saved_dir = ResolveBody(FilePathe)(response.json())
-    assert saved_saved_dir == saved_dir
-    
-    response = await get(base_url=base_url, path='/mothership/clear')
-    assert response.status_code == 200, 'failed to get clear mothership'
-    response = await get(base_url=base_url, path='/jobs/stop') # likely already stopped
+    await add_action(base_url=base_url, name='foo', action=action)
+    await add_scheduler(base_url=base_url, name='bar', scheduler=scheduler)
+    await schedule_action(base_url=base_url, action_name='foo', scheduler_name='bar')
+    await assert_job_count(base_url, 1)
 
-    response = await put(base_url=base_url, path='/actions/foo', data=action)
-    assert response.status_code == 200, f"failed to put action (foo)"
-    response = await get(base_url, path='/actions/foo')
-    assert response.status_code == 200, f"failed to get action (foo)"
-    retrieved_action = ResolveBody(Action)(response.json())
-    assert isinstance(retrieved_action, Action), str(type(retrieved_action))
-
-    response = await put(base_url=base_url, path='/schedulers/bar', data=scheduler)
-    assert response.status_code == 200, f"failed to put scheduler (bar)"
-    response = await get(base_url, path='/schedulers/bar')
-    assert response.status_code == 200, f"failed to get scheduler (bar)"
-    retrieved_scheduler = ResolveBody(Scheduler)(response.json())
-    assert isinstance(retrieved_scheduler, Scheduler), str(type(retrieved_scheduler))
-
-    response = await get(base_url=base_url, path='/schedulers/bar/actions/foo')
-    assert response.status_code == 200, f"failed to schedule action (foo) using scheduler (bar)"
-
-    response = await get(base_url, '/jobs/count')
-    assert response.status_code == 200, 'failed to retrieve job count'
-    job_count = response.json()['job_count']
-    assert job_count == 1, 'expecting a job count of 1'
-
-    await get(base_url, '/jobs/start')
-    time.sleep(4)
-    await get(base_url, '/jobs/stop')
-   
+    await start_and_stop_jobs(base_url, 4)
     lines = None
     with open(action.file, 'r') as fid:
         lines = fid.readlines()
-
     assert lines is not None and isinstance(lines, list) and len(lines) >= 1
 
+# helpers
 
+async def add_action(base_url:str, name:str, action:Action):
+    """ add an action and confirm """
+    response = await put(base_url=base_url, path=f"/actions/{name}", data=action)
+    assert response.status_code == 200, f"failed to put action ({name})"
+    response = await get(base_url, path=f"/actions/{name}")
+    assert response.status_code == 200, f"failed to get action ({name})"
+    retrieved_action = ResolveBody(Action)(response.json())
+    assert isinstance(retrieved_action, Action), str(type(retrieved_action))
 
+async def add_scheduler(base_url:str, name:str, scheduler:Scheduler):
+    """ add a scheduler and confirm """
+    response = await put(base_url=base_url, path=f"/schedulers/{name}", data=scheduler)
+    assert response.status_code == 200, f"failed to put scheduler ({name})"
+    response = await get(base_url, path=f"/schedulers/{name}")
+    assert response.status_code == 200, f"failed to get scheduler ({name})"
+    retrieved_scheduler = ResolveBody(Scheduler)(response.json())
+    assert isinstance(retrieved_scheduler, Scheduler), str(type(retrieved_scheduler))
 
+async def schedule_action(base_url:str, scheduler_name:str, action_name:str):
+    """ schedule an action """
+    response = await get(base_url=base_url, path=f"/schedulers/{scheduler_name}/actions/{action_name}")
+    assert response.status_code == 200, f"failed to schedule action ({action_name}) using scheduler ({scheduler_name})"
 
+async def reset_mothership_continuous(base_url:str, tmp_dir:str):
+    """
+    usage: reset_mothership_continuous(base_url, str(tmp_path))
+    """
 
+    # set saved_dir to fixture, tmp_path (see usage)
+    saved_dir = FilePathe(path=tmp_dir)
+    response = await put(base_url=base_url, path='/mothership/saved_dir', data=saved_dir)
+    assert response.status_code == 200, 'failed to set saved_dir'
+    response = await get(base_url=base_url, path='/mothership/saved_dir')
+    assert response.status_code == 200, 'failed to get saved_dir'
+    saved_saved_dir = ResolveBody(FilePathe)(response.json())
+    assert saved_saved_dir == saved_dir
 
+    # empty the mothership and stop the jobs if they're running
+    # for uvicorn tests to run in a bunch, these objects need to be 'reset' since
+    # evidently the app is shared during the running of the tests
+    response = await get(base_url=base_url, path='/mothership/clear')
+    assert response.status_code == 200, 'failed to get clear mothership'
+    response = await get(base_url=base_url, path='/jobs/stop') # likely already stopped
 
-def compute_base_url(host, port):
-    return f"http://{host}:{port}"
+async def assert_job_count(base_url:str, n:int):
+    response = await get(base_url, '/jobs/count')
+    assert response.status_code == 200, 'failed to retrieve job count'
+    job_count = response.json()['job_count']
+    assert job_count == n, f"expected a job count of ({n})"
+
+async def start_and_stop_jobs(base_url:str, pause:int):
+    await get(base_url, '/jobs/start')
+    time.sleep(pause)
+    await get(base_url, '/jobs/stop')
 
 async def get(base_url:str, path:str):
     async with AsyncClient(base_url=base_url) as ac:
